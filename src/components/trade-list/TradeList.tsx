fc9aa7b1633ead01a6aeec2f380b5738fab08f30
Dan O'Dea <dodea@Dans-MacBook-Air.local>
2023-05-12 16:06:20 -0400

    some bad searching/filtering

--- src/components/trade-list/TradeList.tsx
+++ src/components/trade-list/TradeList.tsx
@@ -1,12 +1,61 @@
 import { Trade } from "../trade/Trade";
 import tradeData from "../../data/MOCK_DATA.json";
+import { useState } from "react";
 
 export function TradeList() {
+  const [searchString, setSearchString] = useState("");
+  const [archived, setArchived] = useState(false);
+  const [shipped, setShipped] = useState(false);
+
+  const searchUsernames = () => {
+    console.log(searchString);
+  };
+
   return (
     <div>
-      {tradeData.map((trade) => {
-        return <Trade trade={trade} />;
-      })}
+      <menu>
+        <label htmlFor="search">
+          Username Search
+          <input
+            name="search"
+            type="text"
+            onBlur={(e) => setSearchString(e.target.value)}
+          />
+          <button onClick={() => searchUsernames()}>Search</button>
+        </label>
+        <label htmlFor="archived">
+          Show archived
+          <input
+            name="archived"
+            type="checkbox"
+            onChange={(e) => setArchived(e.target.checked)}
+          />
+        </label>
+        <label htmlFor="shipped">
+          Show shipped
+          <input
+            name="shipped"
+            type="checkbox"
+            onChange={(e) => setShipped(e.target.checked)}
+          />
+        </label>
+      </menu>
+      {/* {tradeData.map((trade) => {
+        if (!searchString.length || trade.username.includes(searchString)) {
+          return <Trade trade={trade} />;
+        }
+      })} */}
+      {tradeData
+        .filter((trade) => {
+          return (
+            (!searchString.length || trade.username.includes(searchString)) &&
+            (!archived || trade.date.archived) &&
+            (!shipped || trade.date.shipped)
+          );
+        })
+        .map((trade) => {
+          return <Trade trade={trade} />;
+        })}
     </div>
   );
 }
