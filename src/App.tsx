01ce02b70f7c95312925fd4fc82443f68166e5db
Dan O'Dea <danodea@gmail.com>
2023-05-23 15:46:12 -0400

    pull in Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import { TradeListContainer } from "./components/TradeListContainer";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 function App() {
   return (
--- src/components/TradeListMenu.tsx
+++ src/components/TradeListMenu.tsx
@@ -1,3 +1,5 @@
+import Button from "@mui/material/Button";
+
 export interface TradeListMenuProps {
   updateDisplayOptions: (action: string, value: string | boolean) => void;
   handleAddTradeButtonClick: () => void;
@@ -8,12 +10,13 @@ export function TradeListMenu(props: TradeListMenuProps) {
 
   return (
     <form className="mb-4 flex flex-row gap-4 rounded bg-white px-8 py-6 shadow-md">
-      <input
-        className="w-1/6 cursor-pointer rounded bg-cyan-300 px-4 py-2 font-bold hover:bg-cyan-200 active:bg-cyan-600"
+      <Button
+        variant="contained"
         type="button"
         onClick={handleAddTradeButtonClick}
-        value="Add Trade"
-      />
+      >
+        Add Trade
+      </Button>
       <input
         className="ml-auto w-1/4 appearance-none rounded border p-2 text-gray-700 shadow focus:outline-none"
         name="search"
