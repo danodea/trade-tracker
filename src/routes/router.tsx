608be6578114a077d49ca7f816e7c16c7bd7eb18
Dan O'Dea <danodea@gmail.com>
2023-08-15 22:29:36 -0400

    home page

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import { TradeListContainer } from "../components/TradeListContainer";
+import Home from "../components/Home";
 
 export const mainRouter = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element: <Home />,
-  //   },
+  {
+    path: "/",
+    element: <Home />,
+  },
   {
     path: "/trades",
     element: <TradeListContainer />,
